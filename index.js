952f538cad6d5ece7a4e7bda4d9ca5e83442a151
aniwane <aniwane@osidigital.com>
2023-12-30 01:54:13 +0530

    day1

--- index.js
+++ index.js
@@ -45,6 +45,52 @@ app.post("/api/courses", (req, res) => {
   res.send(course);
 });
 
+//PUT
+app.put("/api/courses/:id", (req,res) => {
+
+  const courseId = parseInt(req.params.id);
+  const newName = req.body.name;
+
+  // Find the course by id
+  const courseToUpdate = courses.find(course => course.id === courseId);
+
+  if (!courseToUpdate) {
+    return res.status(404).send('Course not found');
+  }
+
+  // Update the course name
+  courseToUpdate.name = newName;
+
+  // Send a response
+  res.send(courseToUpdate);
+
+});
+
+//DELETE
+app.delete("/api/courses/:id", (req, res) => {
+  const courseId = parseInt(req.params.id);
+
+  // Find the index of the course by id
+  const courseIndex = courses.findIndex(course => course.id === courseId);
+
+  if (courseIndex === -1) {
+    return res.status(404).send('Course not found');
+  }
+
+  // Remove the course from the array
+  const deletedCourse = courses.splice(courseIndex, 1)[0];
+
+  // Send a response with the deleted course
+  res.send(deletedCourse);
+});
+
+function ValidateCourse(course) {
+  if (!course.name || course.name < 3) {
+    res.status(400).send("Name req min 3 characters");
+    return;
+  }
+}
+
 //PORT
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening the port ${port}......`));
