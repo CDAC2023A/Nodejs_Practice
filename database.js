fdcd83ab67e19d8a1e5bf7d97d9d1ba95f0dc8b0
aniwane <aniwane@osidigital.com>
2023-12-30 23:14:05 +0530

    database connection

---
+++ database.js
@@ -0,0 +1,29 @@
+const { error, warn } = require("console");
+// const mySql=require("mysql")
+//require('dotenv').config();
+
+const mysql = require("mysql2");
+
+const connect = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "mydbspring",
+
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_DATABASE
+});
+
+connect.connect((err) => {
+  if (err) {
+    console.error("Error connecting to the database:", err);
+  } else {
+    console.warn("connected successfully");
+  }
+});
+
+connect.query("select * from users", (err, result) => {
+  console.warn("result", result);
+});
